de3fa0af8edfa1af16920bb0fac583918d9d7130
sgtphips <poposhub@gmail.com>
2017-09-30 00:46:48 -0400

    Added static pages and routes

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+	url(r'^$', views.home, name='home'),
+	url(r'about/', views.about, name='about'),
+	url(r'contact/', views.contact, name='contact'),
+	url(r'donations/', views.donations, name='donations'),
+
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+	return render(request, 'home/home.html', {})
+
+def about(request):
+	return render(request, 'about/about.html', {})
+
+def contact(request):
+	return render(request, 'contact/contact.html', {})
+
+def donations(request):
+	return render(request, 'donations/donations.html', {})
